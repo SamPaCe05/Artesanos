804d6a1ab1ffa1a0aec4f8e2c90f9bcbdac5bcb1
yulian168 <ym67896@gmail.com>
2026-01-20 17:14:12 -0500

    Panel-login

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,29 @@
+import './login.css'
+import loginImg from "../assets/artesanos_logo.jpg"
+
+const Login = () => {
+    return (
+        <>
+            <section className="sec">
+                <img src={loginImg} alt="Login" />
+                <h3>Iniciar Sesión</h3>
+                <div className='user-contain'>
+                    <label htmlFor="" className='lbl'>Nombre de usuario</label>
+                    <input type="text" name="" placeholder="Ingrese su nombre" className='user'/>
+                </div>
+                <div className='passw-contain'>
+                    <label htmlFor="" className='lbl'>Contraseña</label>
+                    <input type="password" name="" placeholder='Ingrese su contraseña' className='passw'/>
+
+                </div>
+                <div>
+                    <button className='iniciar'>Iniciar</button>
+                </div>
+
+
+            </section>
+        </>
+    )
+}
+
+export default Login;
