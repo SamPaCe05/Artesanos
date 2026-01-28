6aa30307585c31eb34568b89b40ccac7b7ae96cb
yulian168 <ym67896@gmail.com>
2026-01-28 15:55:05 -0500

    incrementar-decrementar-productos

--- frontend/src/components/fila_tomar_pedido.jsx
+++ frontend/src/components/fila_tomar_pedido.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './fila_tomar_pedido.css'
 
+
+
+
 const FilaTomarPedido=()=>{
+    const [cnt,setcnt]=useState(0);
+
+    const incrementar=()=>{
+        setcnt((tmp)=>tmp+1);
+    }
+
+    const decrementar=()=>{
+        setcnt((tmp)=>Math.max(0,tmp-1))
+    }
     return(
         <>
             <section className='fila-tomar-pedido-sec'>
-                <div className='fila-tomar-pedido-div-uno'>1</div>  
+                <div className='fila-tomar-pedido-div-uno'>{cnt}</div>  
                 <div className='fila-tomar-pedido-div-dos'>Pizza Espanta Brujas</div>  
-                <div className='fila-tomar-pedido-div-tres'>+</div>  
-                <div className='fila-tomar-pedido-div-cuatro'>-</div>  
+                <div className='fila-tomar-pedido-div-tres' onClick={incrementar}>+</div>  
+                <div className='fila-tomar-pedido-div-cuatro' onClick={decrementar}>-</div>  
                 <div className='fila-tomar-pedido-div-cinco'>$20.000</div>  
             </section>
         </>
