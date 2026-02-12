b9bd8ba2e9cd7355ffa2b028a9e408c19f3c0960
SamPaCe05 <samuelpadilla2605@gmail.com>
2026-02-12 17:09:11 -0500

    toast traer ventas

--- frontend/src/pages/ver_ventas.jsx
+++ frontend/src/pages/ver_ventas.jsx
@@ -1,31 +1,38 @@
-import './ver_ventas.css'
-import BotonPedido from '../components/boton_pedido'
-import { apiRequest } from '../services/api'
-import { useEffect, useState } from 'react'
-import arrow from '../assets/flecha.png'
-import { useNavigate } from 'react-router-dom'
+import "./ver_ventas.css";
+import BotonPedido from "../components/boton_pedido";
+import { apiRequest } from "../services/api";
+import { useEffect, useState } from "react";
+import arrow from "../assets/flecha.png";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 export const formateador = new Intl.NumberFormat("es-CO", {
-    style: "currency",
-    currency: "COP",
-    minimumFractionDigits: 0, 
-  });
+  style: "currency",
+  currency: "COP",
+  minimumFractionDigits: 0,
+});
 const VerVentas = () => {
-    const [pedidos, setPedidos] = useState([]);
-    const [total, setTotal] = useState(0)
-    const navigate=useNavigate();
+  const [pedidos, setPedidos] = useState([]);
+  const [total, setTotal] = useState(0);
+  const navigate = useNavigate();
 
-    const traerVentas = async (inicio, fin) => {
-        try{
-            return await apiRequest(`/api/pedidos/resueltos/cierre/${inicio}/${fin}`, {
-            metodo: "GET"
-        })
-        }catch(err){
-            const msj=String(err?.message||err);
-            if(msj.includes("404"))return [];
-            throw err;
-        }
-        
+  const traerVentas = async (inicio, fin) => {
+    try {
+       return await apiRequest(
+        `/api/pedidos/resueltos/cierre/${inicio}/${fin}`,
+        {
+          metodo: "GET",
+        },
+      );
+    } catch (err) {
+      const msj = String(err?.message || err);
+      if (msj.includes("404")) {
+        toast.error("¡Error al cargar las ventas!");
+        return [];
+      }
+
+      throw err;
     }
+  };
 
   const buscarVentas = async (e) => {
     e.preventDefault();
@@ -57,27 +64,27 @@ const VerVentas = () => {
   }, [pedidos]);
   const precio = 5000;
 
-  
+  return (
+    <>
+      <section className="sec-ver-ventas">
+        <form onSubmit={buscarVentas}>
+          <div className="div-titulo-ver-ventas">
+            <button onClick={() => navigate("/caja")}>
+              <img src={arrow} alt="" />
+            </button>
+            <h2>Historial Pedidos</h2>
+          </div>
 
-    return (
-        <>
-            <section className='sec-ver-ventas'>
-                <form onSubmit={buscarVentas}>
-                    <div className='div-titulo-ver-ventas'>
-                        <button onClick={()=>navigate('/caja')}><img src={arrow} alt="" /></button>
-                        <h2>Historial Pedidos</h2>    
-                    </div>
-                    
-                    <div className='contenedor-fechas'>
-                        <div className='div-fechas'>
-                            <label htmlFor="">Fecha Inicio</label>
-                            <input type="date" name='inicio' className='inputs'/>
-                        </div>
-                        <div className='div-fechas'>
-                            <label htmlFor="">Fecha Fin</label>
-                            <input type="date" name='fin'className='inputs' />
-                        </div>
-                    </div>
+          <div className="contenedor-fechas">
+            <div className="div-fechas">
+              <label htmlFor="">Fecha Inicio</label>
+              <input type="date" name="inicio" className="inputs" />
+            </div>
+            <div className="div-fechas">
+              <label htmlFor="">Fecha Fin</label>
+              <input type="date" name="fin" className="inputs" />
+            </div>
+          </div>
 
           <div className="pedidos-encontrados">
             {pedidos.map((p) => (
